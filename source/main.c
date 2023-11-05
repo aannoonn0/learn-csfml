cd6e980e27920767f6d10272db61720edbfa3422
Anon <main.mike.acc@gmail.com>
2023-11-05 17:21:44 +0100

    drawing a simple sircle on the window

--- source/main.c
+++ source/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <SFML/Graphics.h>
 
+#define CIRCLE_RADIUS 30
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 int main()
 {
-	sfVideoMode mode = { 800, 600, 32 };
+	sfVideoMode mode = { SCREEN_WIDTH, SCREEN_HEIGHT, 32 };
 	sfRenderWindow* window;
+	sfCircleShape* circle;
+	sfVector2f circlePos = { SCREEN_WIDTH / 2 - CIRCLE_RADIUS, SCREEN_HEIGHT / 2 - CIRCLE_RADIUS};
 	sfEvent event;
 
 
 	/* Create the main window */
 	window = sfRenderWindow_create(mode, "SFML window", sfResize | sfClose, NULL);
+	circle = sfCircleShape_create();
+
+	sfCircleShape_setPosition(circle, circlePos);
+	sfCircleShape_setRadius(circle, CIRCLE_RADIUS);
+	sfCircleShape_setOutlineThickness(circle, 5);
+	sfCircleShape_setFillColor(circle, sfWhite);
+	sfCircleShape_setOutlineColor(circle, sfBlack);
 
 
 	/* Start the game loop */
@@ -27,9 +39,11 @@ int main()
 		}
 
 		sfRenderWindow_clear(window, sfWhite);
+		sfRenderWindow_drawCircleShape(window, circle, NULL);
 		sfRenderWindow_display(window);
 	}
 
+	sfCircleShape_destroy(circle);
 	sfRenderWindow_destroy(window);
 	return 0;
 }
